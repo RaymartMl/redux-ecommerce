a3410e67b2da571b8c58d58e503188eec18873ed
Raymart Balaguer <Raymart.b123@gmail.com>
2021-02-06 18:11:59 +0800

    feat: :lipstick: Add support for tailwind css
    Using tailiwind official guide: https://tailwindcss.com/docs/guides/create-react-app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
     <div>
-      <h1>Hello there</h1>
+      <Home />
     </div>
   );
 }
