7b17e7662363e1c4f1ca07bca121709ae4051795
Raymart Balaguer <Raymart.b123@gmail.com>
2021-02-13 20:34:07 +0800

    feat: :sparkles: add cart functionality with redux

--- src/store/cart.js
+++ src/store/cart.js
@@ -1,1 +1,33 @@
 // cart -> products + quantity
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: [{ productId: 1, quantity: 1 }],
+  reducers: {
+    addProduct(cart, action) {
+      // payload = {productId, quantity}
+      cart.push(action.payload);
+    },
+
+    updateQuantity(cart, action) {
+      const { productId, quantity } = action.payload;
+      const product = cart.find((product) => product.id === productId);
+
+      if (!product) {
+        return cart.push(action.payload);
+      }
+
+      product.quantity = quantity;
+    },
+
+    removeProduct(products, action) {
+      const { productId } = action.payload;
+      products = products.filter((product) => product.id !== productId);
+    },
+  },
+});
+
+export const { addProduct, updateQuantity, removeProduct } = cartSlice.actions;
+export default cartSlice.reducer;
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productsReducer from "./products";
+import cartReducer from "./cart";
 
 const store = configureStore({
   reducer: {
     products: productsReducer,
+    cart: cartReducer,
   },
 });
 
