459dc9e4a3c50a06922446398c0ca95345d99602
Raymart Balaguer <Raymart.b123@gmail.com>
2021-03-01 13:41:25 +0800

    feat: :truck: more input counter to its own compoent file

---
+++ src/components/InputCounter.js
@@ -0,0 +1,33 @@
+export default function InputCounter({
+  className,
+  noSubmit,
+  value,
+  onDecrement,
+  onIncrement,
+}) {
+  return (
+    <div
+      className={`inline-flex max-w-xs overflow-hidden bg-offWhite select-none ${className}`}
+    >
+      <button
+        type={noSubmit && "button"}
+        className="p-2 px-4 text-lg font-bold focus:outline-none"
+        onClick={onDecrement}
+      >
+        -
+      </button>
+
+      <div className="flex-1 inline-block  p-2 font-bold text-center outline-none text-primary bg-offWhite">
+        {value}
+      </div>
+
+      <button
+        type={noSubmit && "button"}
+        className="p-2 px-4 text-lg font-bold outline-none focus:outline-none"
+        onClick={onIncrement}
+      >
+        +
+      </button>
+    </div>
+  );
+}
