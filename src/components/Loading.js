c10ba689a3cc9b81e12d41a65d65d015fbf15e81
Raymart Balaguer <Raymart.b123@gmail.com>
2021-03-03 17:54:31 +0800

    feat: :lipstick: add y-padding to loading component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,7 +3,7 @@ import { AiOutlineLoading } from "react-icons/ai";
 
 export default function Loading() {
   return (
-    <div className="text-center flex flex-col justify-center items-center">
+    <div className="text-center py-20 flex flex-col justify-center items-center">
       <AiOutlineLoading className="animate-spin text-primary " size="3rem" />
       <p className="text-xl mt-5">Loading Data Please Wait ...</p>
     </div>
