a476df106c4535c8206e82aed8e93773f19b50ff
Raymart Balaguer <Raymart.b123@gmail.com>
2021-03-22 19:55:05 +0800

    feat: :label: add typing data in utils file

--- src/utils/humanReadableNumber.ts
+++ src/utils/humanReadableNumber.ts
@@ -1,9 +1,8 @@
 // https://dev.to/thomasxbanks/human-readable-numbers-58ho
 // make a comma separated number
 // will return a string value
-export function humanReadable(value, lang = null) {
+export function humanReadable(value: number, lang = null) {
   if (!value) return;
   const locale = lang || document.documentElement.lang || "en";
-  const number = parseFloat(value, 10);
-  return number.toLocaleString(locale);
+  return value.toLocaleString(locale);
 }
--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,8 +1,8 @@
-export function getLocalStorage(key, defaultValue) {
+export function getLocalStorage(key: string, defaultValue: any) {
   const value = window.localStorage.getItem(key);
   return value !== null ? JSON.parse(value) : defaultValue;
 }
 
-export function updateLocalStorage(key, value) {
+export function updateLocalStorage(key: string, value: any) {
   window.localStorage.setItem(key, JSON.stringify(value));
 }
